abb4e2cc582d9275794ef0d377d4cf5220f4522d
thuyngavo <lisa.vo49@gmail.com>
2019-04-30 16:03:18 -0500

    trying a different port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 //==========================================================================================
 //serve static content for the app from the "public" directory in the application directory.
