28a995e6375be8a71f1286938070f1bac14c4d11
thuyngavo <lisa.vo49@gmail.com>
2019-04-30 21:35:17 -0500

    adding code in connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -6,24 +6,36 @@ var mysql = require("mysql");
 //==========================================================================================
 //create connection to mysql database
 //==========================================================================================
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Rememberme!1",
-  database: "burgers_db"
-});
+// var connection;= mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Rememberme!1",
+//   database: "burgers_db"
+// });
+
+if (process.env.JAWDB_URL){
+  connection = sql.createConnection(process.env.JAWDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Rememberme!1",
+    database: "burgers_db"
+  });
+};
 
 //==========================================================================================
 //initiate connection to mysql
 //==========================================================================================
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+connection.connect();
 
 //==========================================================================================
 //export connection for orm.js to use
